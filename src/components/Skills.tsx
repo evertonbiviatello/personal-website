d999989c2b36c1083ca307dc0ac335be811091fd
Everton Biviatello <evertonbiviatello@gmail.com>
2024-04-13 16:41:32 -0400

    remove unused imports

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,4 @@
-import { CheckBadgeIcon, CpuChipIcon } from "@heroicons/react/20/solid";
-import HTML from '../assets/html.png';
-import CSS from '../assets/css.png';
-import JavaScript from '../assets/javascript.png';
-import ReactImg from '../assets/react.png';
-import Node from '../assets/node.png';
-import FireBase from '../assets/firebase.png';
-import GitHub from '../assets/github.png';
-import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
+import { CheckBadgeIcon } from "@heroicons/react/20/solid";
 
 export const skills = [
     "JavaScript",
